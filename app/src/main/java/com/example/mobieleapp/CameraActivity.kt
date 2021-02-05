790a4681205ed41f48c11ac93f94c84baf2fe458
joeri1996 <sprengersjoeri1996@gmail.com>
2021-02-05 14:17:33 +0100

    General - Test if camera works

--- app/src/main/java/com/example/mobieleapp/CameraActivity.kt
+++ app/src/main/java/com/example/mobieleapp/CameraActivity.kt
@@ -1,58 +1,42 @@
 package com.example.mobieleapp
 
-
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import io.fotoapparat.Fotoapparat
-import io.fotoapparat.configuration.CameraConfiguration
-import io.fotoapparat.log.logcat
-import io.fotoapparat.log.loggers
-import io.fotoapparat.parameter.ScaleType
-import io.fotoapparat.selector.*
-import io.fotoapparat.view.CameraView
-import java.io.File
 
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_camera.*
 
 class CameraActivity : AppCompatActivity() {
-
-    private val cameraRequest = 1888
-    lateinit var imageView: ImageView
-
+    lateinit var imagview : ImageView
+    lateinit var btnCapture: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
+       var intent : Intent
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        title = "KotlinApp"
-        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
-            == PackageManager.PERMISSION_DENIED)
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequest)
-        imageView = findViewById(R.id.imageView)
-        val photoButton: Button = findViewById(R.id.button)
-        photoButton.setOnClickListener {
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(cameraIntent, cameraRequest)
+        imagview = findViewById(R.id.iv_camera)
+        btnCapture= findViewById(R.id.btnCapture)
+
+        btnCapture.setOnClickListener{
+            intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent,1000)
         }
+
+
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        var bitmap: Bitmap
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == cameraRequest) {
-            val photo: Bitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(photo)
+
+        if(requestCode == 1000 && resultCode == RESULT_OK){
+            bitmap = data?.extras?.get("data") as Bitmap
+            imagview.setImageBitmap(bitmap)
         }
     }
 }
